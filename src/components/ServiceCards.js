be96934e6c45517a3b0ea5d797cc588c0d70ce8e
Rahul <chrahulofficial@gmail.com>
2024-12-03 17:44:43 +0530

    dn

--- src/components/ServiceCards.js
+++ src/components/ServiceCards.js
@@ -1,26 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function ServiceCards({tittle,data}) {
+export default function ServiceCards({ tittle, data }) {
   return (
     <>
       <div className="w-[80vw] mx-auto my-14">
         <div>
-          <h1 className="text-2xl md:text-3xl font-semibold">
-            {tittle}
-          </h1>
-          <div className="flex-col md:flex-row flex items-center gap-12 mt-10">
+          <h1 className="text-2xl font-semibold md:text-3xl">{tittle}</h1>
+          <div className="flex flex-col items-center gap-12 mt-10 md:flex-row">
             {data.map((i, id) => {
               return (
                 <React.Fragment key={id}>
-                  <div className="space-y-1 ">
-                    <img
-                      src={i.img}
-                      alt=""
-                      className="cursor-pointer rounded-lg w-[60vw]"
-                    />
-                    <h1 className="font-semibold">{i.tittle}</h1>
-                    <p className="text-gray-700">Rs {i.price}</p>
-                  </div>
+                  <Link to={`/service/${tittle}`}>
+                    <div className="space-y-1 ">
+                      <img
+                        src={i.img}
+                        alt=""
+                        className="cursor-pointer rounded-lg w-[60vw]"
+                      />
+                      <h1 className="font-semibold">{i.tittle}</h1>
+                      <p className="text-gray-700">Rs {i.price}</p>
+                    </div>
+                  </Link>
                 </React.Fragment>
               );
             })}
